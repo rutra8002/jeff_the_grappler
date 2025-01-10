1503ffe3a0f85517421f998327f29201b5e7db92
ruter <arturkummer08@gmail.com>
2025-01-10 22:41:32 +0100

    added shader cache, so they dont need to be compiled again

--- shaders.py
+++ shaders.py
@@ -1,8 +1,10 @@
 import raylib
+import os
+import pickle
 
 shaders = {}
-
 shaders_enabled = True
+shader_cache_file = 'shaders/shader_cache.pkl'
 
 def load_shader_with_error_check(vs_path, fs_path):
     if fs_path not in shaders:
@@ -10,9 +12,25 @@ def load_shader_with_error_check(vs_path, fs_path):
         if shader.id == 0:
             raise ValueError(f"Failed to load shader from {fs_path}")
         shaders[fs_path] = shader
+        save_shader_cache()
     return shaders[fs_path]
 
+def save_shader_cache():
+    with open(shader_cache_file, 'wb') as cache_file:
+        pickle.dump(list(shaders.keys()), cache_file)
+
+def load_shader_cache():
+    if os.path.exists(shader_cache_file):
+        with open(shader_cache_file, 'rb') as cache_file:
+            cached_shaders = pickle.load(cache_file)
+            for fs_path in cached_shaders:
+                if isinstance(fs_path, str):
+                    fs_path = fs_path.encode('utf-8')
+                shader = raylib.LoadShader(b"", fs_path)
+                shaders[fs_path] = shader
+
 def load_shaders():
+    load_shader_cache()
     try:
         shaders["lava"] = load_shader_with_error_check(b"", b"shaders/lava.fs")
         shaders["background"] = load_shader_with_error_check(b"", b"shaders/background.fs")
@@ -20,4 +38,3 @@ def load_shaders():
         shaders["lighting"] = load_shader_with_error_check(b"", b"shaders/lighting.fs")
     except Exception as e:
         raise e
-
